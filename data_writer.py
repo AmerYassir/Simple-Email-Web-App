fa7dbbc02c9743e8f1eaeb8dac13cf72511f2207
Amer_Yasser <amer.yassir.abdaljalil@gmail.com>
2024-04-17 10:39:00 +0200

    Added data access layer

---
+++ data_writer.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class DataWriter:
+    def __init__(self, db_name='email_app.db'):
+        self.db_name = db_name
+
+    def write_user(self, username, password, email):
+        try:
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
+
+                cursor.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                               (username, password, email))
+
+            print("User data successfully written to the database.")
+        except sqlite3.Error as e:
+            print("Error writing user data to the database:", e)
+
+    def write_message(self, sender_id, receiver_id, subject, body):
+        try:
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
+
+                cursor.execute("INSERT INTO messages (sender_id, receiver_id, subject, body) VALUES (?, ?, ?, ?)",
+                               (sender_id, receiver_id, subject, body))
+
+            print("Message data successfully written to the database.")
+        except sqlite3.Error as e:
+            print("Error writing message data to the database:", e)
