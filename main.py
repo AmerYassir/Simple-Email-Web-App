e84e28835e09250dbc47ac1d964d1638d2667876
Amer_Yasser <amer.yassir.abdaljalil@gmail.com>
2024-04-17 11:58:18 +0200

    Update: added sign up - sign in routes

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import flask 
-from flask import render_template
-app=flask.Flask(__name__)
+from flask import Flask, g,render_template,request
+from data_base import EmailDB
+from data_access import DataAccess
+from data_writer import DataWriter
+
+app=Flask(__name__)
+
+email_db = EmailDB()
+data_writer=DataWriter()
+data_getter=DataAccess()
+
+
+@app.route('/')
+def signup():
+    return render_template('sign_up.html')
+
+@app.route('/signin')
+def sign_in():
+    return render_template('sign_in.html')
 
 @app.route('/inbox')
-@app.route('/')
 def inbox():
     return render_template('inbox.html')
 
@@ -11,7 +27,22 @@ def inbox():
 def draft():
     return render_template('draft.html')
 
-@app.route('/send')
+@app.route('/send',methods=['POST','GET'])
 def send():
+    reciver= request.form['to']
+    subject= request.form['subject']
+    message= request.form['message']
+    
     return render_template('send.html')
 
+@app.before_request
+def before_request():
+    g.db = email_db.get_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    email_db.close_db()
+
+if __name__ == "__main__":
+    email_db.init_db()
+    app.run(debug=True)
