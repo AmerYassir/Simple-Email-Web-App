f565719ff2a48bf2c3bf9f4a3e374c371ac6d144
Amer_Yasser <amer.yassir.abdaljalil@gmail.com>
2024-04-18 17:12:44 +0200

    UPDATE: added sending emails logic

--- data_access.py
+++ data_access.py
@@ -4,11 +4,11 @@ class DataAccess:
     def __init__(self, db_name='email_app.db'):
         self.db_name = db_name
 
-    def get_user_by_username(self, username):
+    def get_user_by_email(self, email):
         try:
             with sqlite3.connect(self.db_name) as conn:
                 cursor = conn.cursor()
-                cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+                cursor.execute("SELECT * FROM users WHERE email=?", (email,))
                 user = cursor.fetchone()
                 return user
         except sqlite3.Error as e:
