d37a6ad70fbe2bea616a1fd0cb90201c49dd357c
Amer_Yasser <amer.yassir.abdaljalil@gmail.com>
2024-04-27 23:09:49 +0300

    enhanced UI by tracking current category & fixed text lentgh issue

---
+++ static/base.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current URL path
+    var path = window.location.pathname;
+    // Get the navigation links
+    var navItems = document.querySelectorAll('.nav-item');
+
+    // Loop through each navigation link
+    navItems.forEach(function(item) {
+        // Check if the link's href matches the current URL path
+        if ("/"+item.getAttribute('id') === path) {
+            // Add the active class to the matched link
+            item.classList.add('active');
+            console.log('done')
+        }
+    });
+});
